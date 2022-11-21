926996fb13cea999cda56b23bb6dd9ef1c7e93d9
Roman Burko <redstone.kem@gmail.com>
2022-11-21 16:11:43 +0300

    added layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,8 @@
-<h1>О блоге</h1>
+@extends('layouts.app')
+
+@section('header', 'О блоге')
+
+@section('content')
 <p>Эксперименты с Laravel на Хекслете</p>
 <p>{{ implode(', ', $tags) }}</p>
+@endsection
