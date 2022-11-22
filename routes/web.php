7c28d8b31603c721f834c758bdb676a2ab0d84f9
Roman Burko <redstone.kem@gmail.com>
2022-11-22 19:09:52 +0300

    add index route for articles

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 });
 
 Route::get('about', [PageController::class, 'about']);
+
+Route::get('articles', [ArticleController::class, 'index'])
+    ->name('articles.index');
